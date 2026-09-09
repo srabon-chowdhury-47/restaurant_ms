497175daf7744f1f7aa71ec4ea651b31f28f3249
Md Srabon Chowdhury <fenixdy47@gmail.com>
2026-09-09 20:22:38 +0600

    feat: update axios instance to use environment variable for base URL

--- client/src/apis/axiosinstance.jsx
+++ client/src/apis/axiosinstance.jsx
@@ -1,8 +1,6 @@
 import axios from "axios";
 
-const API_PORT = 8000;
-
-const BASE_URL = `http://${window.location.hostname}:${API_PORT}/api`;
+const BASE_URL = `${import.meta.env.VITE_API_URL}/api`;
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
